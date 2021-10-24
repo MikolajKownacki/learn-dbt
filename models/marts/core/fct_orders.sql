e28677e4d85deee0ddc4de7b2ba9b531b0c12578
mikolaj.kownacki@payu.com <mikolaj.kownacki@payu.com>
2021-10-24 18:48:39 +0000

    tests

---
+++ models/marts/core/fct_orders.sql
@@ -0,0 +1,10 @@
+with payments as (
+    select * from {{ ref('stg_payments')}}
+),
+
+orders as (
+    select * from {{ ref('stg_orders')}}
+)
+select orders.order_id, orders.customer_id, payments.amount
+from orders
+left join payments on orders.order_id = payments.order_id
---
+++ models/stg/jaffle_shop/stg_customers.sql
@@ -0,0 +1,12 @@
+with 
+customers as (
+
+    select
+        id as customer_id,
+        first_name,
+        last_name
+
+    from dbt_test.jaffle_shop_customers
+
+)
+select * from customers
